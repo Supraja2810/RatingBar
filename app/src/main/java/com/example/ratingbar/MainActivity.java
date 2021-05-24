3486a58ddd89fc253a63d3bb2ee4711a2e8a9a9a
Supraja2810 <suprajabalu28@gmail.com>
2021-05-24 13:41:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ratingbar/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.ratingbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    RatingBar ratingBar;
+    Button btSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ratingBar = findViewById(R.id.ratingBar);
+        btSubmit = findViewById(R.id.submit);
+
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = String.valueOf(ratingBar.getRating());
+                Toast.makeText(getApplicationContext(),s+" Stars!"
+                ,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
